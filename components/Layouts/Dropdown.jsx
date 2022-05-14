d4fbb30b66c911aad357274e2dd886d22582187a
Ali <ali.text77@gmail.com>
2022-05-14 08:39:32 +0430

    improve UI

--- components/Layouts/Dropdown.jsx
+++ components/Layouts/Dropdown.jsx
@@ -10,7 +10,7 @@ const Dropdown = ({ setUserDropdownVisibility, handleLogout }) => {
 
     return (
         <OutsideClickHandler onOutsideClick={() => setUserDropdownVisibility(false)}>
-            <motion.div animate={{ y: [-100, 0] }} exit={{ y: [0, -100] }} className="absolute top-8 lg:top-10 left-0 z-20 w-24 bg-slate-100 rounded-lg drop-shadow-lg p-2" onClick={() => closeAndLogout()}>
+            <motion.div animate={{ y: [-100, 0] }} exit={{ y: [0, -100] }} className="absolute top-8 lg:top-12 left-0 z-20 w-24 bg-slate-100 rounded-lg drop-shadow-lg p-2" onClick={() => closeAndLogout()}>
                 <button className="text-xs w-full flex text-right gap-x-2 hover:text-red-500 hover-transition">
                     <i className="fa-light fa-door-open text-base"></i>
                     <span>خروج</span>
